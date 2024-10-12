0202bfb3f7a78fc5dbca8210969396e34bc9aacf
Lazizbek Usmanov <149256879+ssaintx@users.noreply.github.com>
2024-10-12 23:06:15 +0500

    dev.nix

---
+++ dev.nix
@@ -0,0 +1,41 @@
+{ pkgs }: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+      "bradlc.vscode-tailwindcss"
+      "Catppuccin.catppuccin-vsc-icons"
+      "christian-kohler.npm-intellisense"
+      "christian-kohler.path-intellisense"
+      "dsznajder.es7-react-js-snippets"
+      "FittenTech.Fitten-Code"
+      "GitHub.github-vscode-theme"
+      "jolaleye.horizon-theme-vscode"
+      "mhutchie.git-graph"
+      "MoBalic.jetbrains-dark-theme"
+      "Pilleye.dusk-theme"
+      "PulkitGangwar.nextjs-snippets"
+      "sndst00m.vscode-native-svg-preview"
+      "streetsidesoftware.code-spell-checker"
+      "streetsidesoftware.code-spell-checker-russian"
+      "wix.vscode-import-cost"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
